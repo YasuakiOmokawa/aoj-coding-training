cc2ee016473ba7db7fb470d8603f6d11fe5d32f7
面川泰明 <omokawa@senk-inc.co.jp>
2020-02-14 10:04:41 +0900

    practice c++ coding

--- alds1_1_d.cpp
+++ alds1_1_d.cpp
@@ -4,7 +4,20 @@ using namespace std;
 static const int MAX = 200000;
 
 int main() {
-  int R[MAX], n;
+  // int R[MAX], n;
 
-  
+  // cin >> n;
+  // for (int i = 0; i < n; i++) cin >> R[i];
+  cout << "hello\n";
+
+  double height = 1.71;
+  double mass = 77.4;
+  double bmi = mass / (height*height);
+
+  cout << "BMI=" << bmi << "\n";
+
+  auto status = [](double bmi)
+  {
+    
+  }
 }
