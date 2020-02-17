0582d712f79d4b593c2a0fbe6394c9b042510705
面川泰明 <omokawa@senk-inc.co.jp>
2020-02-17 09:51:05 +0900

    Practice using function

--- alds1_1_d.cpp
+++ alds1_1_d.cpp
@@ -1,23 +1,18 @@
-#include<iostream>
-#include<algorithm>
+#include <iostream>
+#include <algorithm>
 using namespace std;
 static const int MAX = 200000;
 
-int main() {
+int plus(int x, int y)
+{
+  return x + y;
+}
+
+int main()
+{
   // int R[MAX], n;
 
   // cin >> n;
   // for (int i = 0; i < n; i++) cin >> R[i];
-  cout << "hello\n";
-
-  double height = 1.71;
-  double mass = 77.4;
-  double bmi = mass / (height*height);
-
-  cout << "BMI=" << bmi << "\n";
-
-  auto status = [](double bmi)
-  {
-    
-  }
+  auto x = [](int x, int y);
 }
