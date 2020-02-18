f1ac218e74190e9fec48254bb70c114bd772d6a2
面川泰明 <omokawa@senk-inc.co.jp>
2020-02-18 11:03:10 +0900

    increase func definition

--- alds1_1_d.cpp
+++ alds1_1_d.cpp
@@ -3,16 +3,15 @@
 using namespace std;
 static const int MAX = 200000;
 
-int plus(int x, int y)
-{
-  return x + y;
-}
-
 int main()
 {
   // int R[MAX], n;
 
   // cin >> n;
   // for (int i = 0; i < n; i++) cin >> R[i];
-  auto x = [](int x, int y);
+  auto calc = [](int x, int y) {
+    return x + y;
+  };
+
+  cout << calc(1, 2) << "\n";
 }
