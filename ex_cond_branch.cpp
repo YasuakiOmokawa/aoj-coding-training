016cee64f5227d001fd1481dfa11954afb7e87b0
面川泰明 <omokawa@senk-inc.co.jp>
2020-02-19 09:57:28 +0900

    Is not function

--- ex_cond_branch.cpp
+++ ex_cond_branch.cpp
@@ -2,13 +2,19 @@
 #include <algorithm>
 using namespace std;
 
+// ロケットが発射可能かどうかを返す関数
+bool is_rocket_ready_to_launch()
+{
+  // まだだよ
+  return false;
+}
+
 int main()
 {
-  std::cout << std::boolalpha;
 
-  // false
-  std::cout << !true << "\n";
-
-  // true
-  std::cout << !false << "\n";
+  // ロケットが発射可能ではないときに実行される
+  if (!is_rocket_ready_to_launch())
+  { // もうしばらくそのままでお待ちください
+    std::cout << "Standby...\n";
+  }
 }
