75dacd0e005ed8dc7372a315012eab7c99a4f2dd
面川泰明 <omokawa@senk-inc.co.jp>
2020-02-19 09:43:34 +0900

    Compare string size

--- ex_cond_branch.cpp
+++ ex_cond_branch.cpp
@@ -4,9 +4,27 @@ using namespace std;
 
 int main()
 {
-  if (1 < 2)
+  auto cat = "cat";
+  auto dog = "dog";
+
+  if (cat < dog)
   {
-    std::cout << "yes!\n";
-    std::cout << "yes!\n";
+    cout << "cat is smaller\n";
   }
+  else
+  {
+    cout << "dog is smaller\n";
+  }
+
+  auto longcat = "longcat";
+
+  if (longcat > cat)
+  {
+    cout << "Longcat is long\n";
+  }
+  else
+  {
+    cout << "Longcat isn't that long. Sign\n";
+  }
+
 }
