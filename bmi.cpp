83bdfd8e3f1233bc7ee66f6ce978cb7cb0302c46
面川泰明 <omokawa@senk-inc.co.jp>
2020-02-20 09:45:20 +0900

    Create bmi.cpp

---
+++ bmi.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+  // 身長1.63m
+  double height = 1.63;
+  // 体重73kg
+  double mass = 73.0;
+
+  // BMIの計算
+  double bmi = mass / (height * height);
+
+  // BMIの出力
+  std::cout << "BMI=" << bmi << "\n";
+
+  // 状態の判定をする関数
+  auto status = [](double bmi) {
+    if (bmi < 18.5)
+      return "Underweight.\n";
+    else if (bmi < 25.0)
+      return "Normal.\n";
+    else if (bmi < 30.0)
+      return "Overweight.\n";
+    else
+      return "Obese.";
+  };
+
+  // 状態の出力
+  std::cout << status(bmi);
+}
