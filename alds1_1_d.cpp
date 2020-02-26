d5fdc5dc4cd47183c8dea406471ea9cfff2dbd4e
面川泰明 <omokawa@senk-inc.co.jp>
2020-02-26 11:30:49 +0900

    Complete algorithm

--- alds1_1_d.cpp
+++ alds1_1_d.cpp
@@ -5,13 +5,22 @@ static const int MAX = 200000;
 
 int main()
 {
-  // int R[MAX], n;
+  int R[MAX], n;
 
-  // cin >> n;
-  // for (int i = 0; i < n; i++) cin >> R[i];
-  auto calc = [](int x, int y) {
-    return x + y;
-  };
+  cin >> n;
+  for (int i = 0; i < n; i++)
+    cin >> R[i];
 
-  cout << calc(1, 2) << "\n";
+  int maxv = -2000000000; // 充分に小さい初期値を設定
+  int minv = R[0];
+
+  for (int i = 1; i < n; i++)
+  {                                // ここで毎回R[i]を読み込めば、配列は不要
+    maxv = max(maxv, R[i] - minv); // 最大値の更新
+    minv = min(minv, R[i]);        // ここまでの最小値を保持しておく
+  }
+
+  cout << maxv << endl;
+
+  return 0;
 }
