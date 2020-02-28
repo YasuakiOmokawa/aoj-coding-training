c100015714ea3d27a1e1a119bca68764ff5a0d73
面川泰明 <omokawa@senk-inc.co.jp>
2020-02-28 10:10:36 +0900

    infinite while loop

--- loop.cpp
+++ loop.cpp
@@ -2,26 +2,10 @@
 #include <algorithm>
 using namespace std;
 
-void hello_n(int n)
-{
-  if (n < 0)
-  {
-    return;
-  }
-
-  int i = 0;
-loop:
-  if (i != n)
-  {
-    cout << "hello\n";
-    i++;
-    goto loop;
-  }
-};
-
 int main()
 {
-  int n{};
-  cin >> n;
-  hello_n(n);
+  auto hello = []() { cout << "hello\n"; };
+
+  while (true)
+    hello();
 }
