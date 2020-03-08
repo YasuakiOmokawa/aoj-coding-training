7869bed4ed98b46dd684e2542e4d55c8aeceff09
面川泰明 <omokawa@senk-inc.co.jp>
2020-03-08 10:52:21 +0900

    Output vector data using at()

--- vector.cpp
+++ vector.cpp
@@ -7,11 +7,13 @@ int main()
 {
   vector<int> v;
 
-  for (int iota = 0; iota != 1000; ++i)
+  for (int iota = 0; iota != 10; ++iota)
   {
-    v.push_back(i);
+    v.push_back(iota);
   }
 
-  cout << v.size() << "\n";
-  cout << v.at(-1) << "\n";
+  for (size_t index = 0; index != v.size(); ++index)
+  {
+    cout << v.at(index) << " ";
+  }
 }
