1aee17734a343f1d7378ca3202900ab13e1a913b
面川泰明 <omokawa@senk-inc.co.jp>
2020-03-10 10:07:02 +0900

    Search minimum data

--- vector.cpp
+++ vector.cpp
@@ -5,12 +5,23 @@ using namespace std;
 
 int main()
 {
-  // item 1,2,3
-  vector<int> v = {1, 2, 3};
+    // vectorの変数をv
+    vector<int> v = { 8, 3, 7, 4, 2, 9, 3 } ;
+    // 要素数をsizeとする
+    size_t size = v.size() ;
 
-  auto x = v.at(0); // 1
-  auto y = v.at(1); // 2
-  auto z = v.at(2); // 3
+    // 変数minに0を代入する
+    size_t min = 0 ;
 
-  cout << x << y << z << "\n";
+    // size回のループを実行する
+    // 変数iに0からsize-1までの整数を代入する
+    for ( size_t index = 1 ; index != size ; ++index )
+    {
+        // 'v.at(index) < v.at(min)'ならばmin = index
+        if ( v.at(index) < v.at(min) )
+            min = index ;
+    }
+
+    // 一番小さい値を出力
+    cout << v.at(min) ;
 }
