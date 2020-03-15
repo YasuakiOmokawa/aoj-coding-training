7c469b278a94986a552aadf30ce18a896824da9d
面川泰明 <omokawa@senk-inc.co.jp>
2020-03-16 08:32:23 +0900

    Setting debug

---
+++ selected_sort.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main()
+{
+  vector<int> v = {3,8,2,5,6,9,4,1,7};
+  auto size = v.size();
+
+  for (size_t head = 0; head != size; ++head)
+  {
+    auto 
+  }
+
+}
