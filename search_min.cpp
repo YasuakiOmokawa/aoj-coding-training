a3dabf7b2caa54b331e872c4142053f55a48a806
面川泰明 <omokawa@senk-inc.co.jp>
2020-03-15 10:29:58 +0900

    Before swap logic

--- search_min.cpp
+++ search_min.cpp
@@ -8,10 +8,13 @@ int main()
   vector<int> v = {8, 3, 7, 4, 2, 9, 3};
   size_t size = v.size();
 
+  for (size_t head = 0; head != size; ++head)
   {
-    size_t min = 0;
+    // 現在の先頭であるmin番目を、仮の最小の要素とみなすのでhead
+    size_t min = head;
 
-    for (size_t index = 1; index != size; ++index)
+    // 現在の先頭の次の要素から探すので、head + 1
+    for (size_t index = head + 1; index != size; ++index)
     {
       if (v.at(index) < v.at(min))
         min = index;
