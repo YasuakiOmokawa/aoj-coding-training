d37b35085acd5b834e3fd79904d2f340580573d8
面川泰明 <omokawa@senk-inc.co.jp>
2020-03-15 10:23:56 +0900

    Progressive code

--- search_min.cpp
+++ search_min.cpp
@@ -5,4 +5,20 @@ using namespace std;
 
 int main()
 {
+  vector<int> v = {8, 3, 7, 4, 2, 9, 3};
+  size_t size = v.size();
+
+  {
+    size_t min = 0;
+
+    for (size_t index = 1; index != size; ++index)
+    {
+      if (v.at(index) < v.at(min))
+        min = index;
+    }
+
+    cout << v.at(min) << " ";
+
+    // 先頭と交換
+  }
 }
