1ff929f97c04461b9f7ff8dce8a053925bf98147
面川泰明 <omokawa@senk-inc.co.jp>
2020-03-17 08:41:13 +0900

    Imprement selected sort with bug

--- selected_sort.cpp
+++ selected_sort.cpp
@@ -5,12 +5,26 @@ using namespace std;
 
 int main()
 {
-  vector<int> v = {3,8,2,5,6,9,4,1,7};
+  vector<int> v = {3, 8, 2, 5, 6, 9, 4, 1, 7};
   auto size = v.size();
 
   for (size_t head = 0; head != size; ++head)
   {
-    auto 
+    auto min = head;
+    for (size_t index = head + 1; index != size; ++index)
+    {
+      if (v.at(index) < v.at(min))
+        min = index;
+    }
+
+    auto temp = v.at(head);
+    v.at(head) = v.at(min);
+    v.at(min) = v.at(head);
   }
 
+  for (size_t i = 0; i != size; ++i)
+  {
+    cout << v.at(i) << " ";
+  }
+  cout << "\n";
 }
