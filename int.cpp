8ed71990494cbf783b93abf84a037673f8bdb681
面川泰明 <omokawa@senk-inc.co.jp>
2020-03-29 10:14:37 +0900

    Out int limits

--- int.cpp
+++ int.cpp
@@ -5,26 +5,7 @@ using namespace std;
 
 int main()
 {
-  int a = 123;
-  int b = 0;
-  int c = -123;
-  auto d = 123u;
-  long aa = 1;
-  unsigned long ab = 1;
-  auto ac = 1'000'000'000;
-  auto ad = 123l;
-  auto ae = 123ul;
-  auto af = 123ull;
-  // cout << sizeof(af) << "\n";
-
-  auto print = [](std::size_t s)
-  { cout << s << "\n"; };
-
-  print(sizeof(char));
-  print(sizeof(short));
-  print(sizeof(int));
-  print(sizeof(long));
-  print(sizeof(long long));
-
-  
+  cout
+      << numeric_limits<int>::min() << "\n"
+      << numeric_limits<int>::max();
 }
