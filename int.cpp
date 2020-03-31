33a025e8a28c09e2b47e6f60e285d70b136dd40f
面川泰明 <omokawa@senk-inc.co.jp>
2020-04-01 08:57:54 +0900

    Check unsigned int

--- int.cpp
+++ int.cpp
@@ -5,7 +5,14 @@ using namespace std;
 
 int main()
 {
-  cout
-      << numeric_limits<unsigned int>::min() << "\n"
-      << numeric_limits<unsigned int>::max();
+  unsigned int min = numeric_limits<unsigned int>::min();
+  unsigned int max = numeric_limits<unsigned int>::max();
+
+  unsigned int min_minus_one = min - 1u;
+  unsigned int max_plus_one = max + 1u;
+
+  cout << min << "\n"
+       << max << "\n"
+       << min_minus_one << "\n"
+       << max_plus_one;
 }
