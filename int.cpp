cd032e86ef94f26ab30e9019fad428758cb41648
面川泰明 <omokawa@senk-inc.co.jp>
2020-04-04 13:53:04 +0900

    overflow short int

--- int.cpp
+++ int.cpp
@@ -5,14 +5,6 @@ using namespace std;
 
 int main()
 {
-  unsigned int min = numeric_limits<unsigned int>::min();
-  unsigned int max = numeric_limits<unsigned int>::max();
-
-  unsigned int min_minus_one = min - 1u;
-  unsigned int max_plus_one = max + 1u;
-
-  cout << min << "\n"
-       << max << "\n"
-       << min_minus_one << "\n"
-       << max_plus_one;
+  short x = 40000;
+  cout << x << "\n";
 }
