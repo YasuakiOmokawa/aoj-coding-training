09517ab83712cb80341e3b94da0fc72ec30b938a
面川泰明 <omokawa@senk-inc.co.jp>
2020-04-21 09:18:26 +0900

    Compare iterator

--- iterator.cpp
+++ iterator.cpp
@@ -7,15 +7,22 @@ int main()
 {
   vector<int> v = {1, 2, 3, 4, 5};
 
-  auto i = begin(v);
+  auto x = begin(v);
+  auto y = x;
 
-  cout << *i; // dereference?
-  ++i;
-  cout << *i;
-  ++i;
-  cout << *i;
+  // x, yは0番目の要素を指す
 
-  for (auto a : v)
+  bool b1 = (x == y);
+  bool b2 = (x != y);
+
+  ++x; // 1番目の要素を指す
+
+  bool b3 = (x == y);
+  bool b4 = (x != y);
+
+  vector<bool> bv = {b1, b2, b3, b4};
+
+  for (auto a : bv)
   {
     cout << a << endl;
   }
