bef000f07d7842949f3def2575c9d44c3e16b640
面川泰明 <omokawa@senk-inc.co.jp>
2020-05-13 09:24:10 +0900

    Initialize a reference

---
+++ lvalue_and_const.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <iterator>
+#include <experimental/filesystem>
+using namespace std;
+
+int main()
+{
+  int a = 1;
+  int b = 2;
+
+  b = a;
+  b = 3;
+}
