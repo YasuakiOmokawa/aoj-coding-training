6f694feab7cb6c9c38579ea662d89227c1d0c27c
面川泰明 <omokawa@senk-inc.co.jp>
2020-05-20 09:21:11 +0900

    Referenced parameter into function

--- lvalue_and_const.cpp
+++ lvalue_and_const.cpp
@@ -5,13 +5,14 @@
 #include <experimental/filesystem>
 using namespace std;
 
+void f(int &x)
+{
+  x = 3;
+}
+
 int main()
 {
   int a = 1;
-  int &ref = a;
-
-  ref = 3;
-
+  f(a);
   cout << a << "\n";
-  cout << ref << "\n";
 }
