269315fc9344ae35c8abdb18cd8f483ca0962613
面川泰明 <omokawa@senk-inc.co.jp>
2020-05-24 13:53:45 +0900

    New example constant reference

---
+++ const_ref.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <iterator>
+#include <experimental/filesystem>
+using namespace std;
+
+void f(std::vector<int> v)
+{
+  cout << v.at(1234);
+}
+
+int main()
+{
+
+  // vector contain 10000 items
+  vector<int> v(10000);
+  f(v);
+
+  int x = 0;
+  x = 1;
+
+  const int y = 0;
+  int const z = 0;
+
+  int &ref = x;
+  ++ref;
+
+  // ok, enable const
+  const int &cref = x;
+  const int &const_ref = x;
+
+  // constant type object
+  const int w = 0;
+  // error, not const
+  // int &w_ref = w;
+  // ok, with const
+  const int &w_cref = w;
+}
