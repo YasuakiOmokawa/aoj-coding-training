c8ba45d517133e9869a71cf6ed04d023099d1744
面川泰明 <omokawa@senk-inc.co.jp>
2020-05-24 11:09:53 +0900

    Same name

---
+++ const.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <iterator>
+#include <experimental/filesystem>
+using namespace std;
+
+int main()
+{
+  int x = 0;
+  x = 1;
+
+  const int y = 0;
+  int const z = 0;
+}
