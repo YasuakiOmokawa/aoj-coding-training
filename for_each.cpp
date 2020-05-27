171a273fdc8bc9769f4df56b3497ee5c053034f2
面川泰明 <omokawa@senk-inc.co.jp>
2020-05-27 09:26:24 +0900

    Create print_all

--- for_each.cpp
+++ for_each.cpp
@@ -5,16 +5,16 @@
 #include <experimental/filesystem>
 using namespace std;
 
+auto print_all = [](auto first, auto last) {
+  for (auto iter = first; iter != last; ++iter)
+  {
+    cout << *iter;
+  }
+};
+
 int main()
 {
   vector<int> v = {1, 2, 3, 4, 5};
 
-  for (
-      auto i = begin(v),
-           j = end(v);
-      i != j;
-      ++i)
-  {
-    cout << *i;
-  }
+  print_all(begin(v), end(v));
 }
