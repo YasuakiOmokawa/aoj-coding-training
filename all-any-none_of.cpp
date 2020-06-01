83baa42a91d4820b61ea6ae689ec5d501f8bd24b
面川泰明 <omokawa@senk-inc.co.jp>
2020-06-01 09:40:02 +0900

    Add sample code

--- all-any-none_of.cpp
+++ all-any-none_of.cpp
@@ -5,4 +5,18 @@
 #include <experimental/filesystem>
 using namespace std;
 
-int main(){};
+int main()
+{
+  auto is_all_of_odd = [](auto first, auto last) {
+    return all_of(first, last, [](auto value) { return value % 2 == 0; });
+  };
+
+  auto is_all_of_le_100 = [](auto first, auto last) {
+    return all_of(first, last, [](auto value) { return value <= 100; });
+  };
+
+  vector<int> v;
+
+  bool b = all_of(begin(v), end(v), [](auto value) { return false; });
+  cout << b << "\n";
+};
