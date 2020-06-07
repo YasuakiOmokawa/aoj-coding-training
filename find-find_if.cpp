7126b3a3ecb9c4a3ecf3f3c8f7d8bf6294553017
面川泰明 <omokawa@senk-inc.co.jp>
2020-06-07 11:18:45 +0900

    Coding find algorithm

--- find-find_if.cpp
+++ find-find_if.cpp
@@ -21,4 +21,23 @@ int main()
   {
     cout << "Not found.\n";
   }
+
+  auto my_find = [](auto first, auto last, auto const &value) {
+    for (auto iter = first; iter != last; ++iter)
+    {
+      if (*iter == value)
+        return iter;
+    }
+    return last;
+  };
+
+  auto my_pos = my_find(begin(v), end(v), 2);
+  if (my_pos != end(v))
+  {
+    cout << "Found.\n";
+  }
+  else
+  {
+    cout << "Not found.\n";
+  }
 }
