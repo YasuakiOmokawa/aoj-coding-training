7d7d00f8d5ab1c4290e059bd9429c4cfee71a54b
面川泰明 <omokawa@senk-inc.co.jp>
2020-06-09 09:53:56 +0900

    Create count-count_if.cpp

---
+++ count-count_if.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <iterator>
+#include <experimental/filesystem>
+using namespace std;
+
+int main()
+{
+  vector<int> v = {1, 2, 1, 1, 3, 3};
+
+  auto a = count(begin(v), end(v), 1);
+  cout << a << "\n";
+
+  auto my_count = [](auto first, auto last, auto value) {
+    auto counter = 0u;
+    for (auto i = first; i != last; ++i)
+  }
+}
