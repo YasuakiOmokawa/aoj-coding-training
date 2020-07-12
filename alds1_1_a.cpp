cd478398524d75fd114eedc9f16cfab390af10bf
面川泰明 <omokawa@senk-inc.co.jp>
2020-07-12 11:10:39 +0900

    Insert array from input values

--- alds1_1_a.cpp
+++ alds1_1_a.cpp
@@ -2,13 +2,21 @@
 #include <algorithm>
 using namespace std;
 
+auto print_value = [](auto value) { cout << value; };
+
 int main()
 {
   int n;
+  cin >> n; // 配列の要素数nを代入
 
-  cin >> n;
+  int A[n];
+  for (int i = 0; i < n; i++)
+    cin >> A[i]; // 入力値を配列Aへ代入
 
-  cout << n << endl;
+  for (int i = 1; i < n - 1; i++)
+  {
+    cout << A[i] << "\n";
+  }
 
   return 0;
 }
