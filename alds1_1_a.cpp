f0f58a437adc7fb1c699c48446f7844fb4f10e3d
面川泰明 <omokawa@senk-inc.co.jp>
2020-07-15 09:40:41 +0900

    Impl insertion sort

--- alds1_1_a.cpp
+++ alds1_1_a.cpp
@@ -15,8 +15,19 @@ int main()
 
   for (int i = 1; i < n - 1; i++)
   {
-    cout << A[i] << endl;
+    int v = A[i];
+    int j = i - 1;
+    while (j >= 0 && A[i] > v)
+    {
+      A[j + 1] = A[j];
+      j--;
+    }
+    A[j + 1] = v;
   }
+
+  // check 
+  for (int i = 0; i < n; i++)
+    cout << A[i] << endl;
 
   return 0;
 }
