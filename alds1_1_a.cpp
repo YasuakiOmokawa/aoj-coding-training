9cdf2a49f139e1dc41d24fd8e31e892d68c684f0
面川泰明 <omokawa@senk-inc.co.jp>
2020-07-21 09:35:24 +0900

    Add print debug

--- alds1_1_a.cpp
+++ alds1_1_a.cpp
@@ -2,7 +2,13 @@
 #include <algorithm>
 using namespace std;
 
-auto print_value = [](auto value) { cout << value; };
+auto print_value = [](auto array, int n) {
+  for (int i = 0; i < n; i++)
+  {
+    cout << array[i] << " ";
+  }
+  cout << endl;
+};
 
 int main()
 {
@@ -12,6 +18,8 @@ int main()
   int A[n];
   for (int i = 0; i < n; i++)
     cin >> A[i]; // 入力値を配列Aへ代入
+
+  print_value(A, n); // check
 
   for (int i = 1; i < n - 1; i++)
   {
@@ -23,11 +31,10 @@ int main()
       j--;
     }
     A[j + 1] = v;
+    print_value(A, n); // check
   }
 
-  // check 
-  for (int i = 0; i < n; i++)
-    cout << A[i] << endl;
+  print_value(A, n); // check
 
   return 0;
 }
